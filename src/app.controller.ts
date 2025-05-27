4b1584eea3391be8d4f1312e3f82950b7f3b03ea
the-anh1210 <the.anh.business1210@gmail.com>
2025-05-27 14:01:06 +0700

    advanced-controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('image')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: memoryStorage(),
+    fileFilter: (req, file, cb) => {
+      if (!file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+        return cb(
+          new Error('Only valid image files are allowed! (.jpg, .jpeg, .png, .gif)'), 
+          false
+        );
+      }
+      cb(null, true);
+    },
+  }))
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.handleUpload(file);
   }
 }
